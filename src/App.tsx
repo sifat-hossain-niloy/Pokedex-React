4cc2957b9dd04e58a8695d56c018532f1e172492
Md. Sifat Hossain <sifatb910@gmail.com>
2023-05-15 03:24:25 +0600

    modified:   package-lock.json     modified:   package.json 	modified:   src/App.tsx 	new file:   src/components/AuthRoute.tsx 	new file:   src/config/config.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import PokemonDetail from "./components/PokemonDetail";
 import Home from "./pages/Home";
 import theme from "./theme";
+import {initializeApp} from "firebase/app"
+import {config} from "./config/config"
+import AuthRoute from "./components/AuthRoute";
+import { Login } from "@mui/icons-material";
+import LoginPage from "./pages/Login";
 
+ initializeApp(config.firebaseConfig);
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
+    element: <AuthRoute> <Home /> </AuthRoute>,
   },
   {
     path: "pokemon/:pokemonName",
     element: <PokemonDetail/>
+  },
+  {
+    path : "/login",
+    element : <LoginPage/>
   }
 ]);
 
---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const  config = {
+        firebaseConfig : {
+
+        apiKey: "AIzaSyCe9TfFxt2swnf1jNUt1eiE6wpH36SZBFs",
+      
+        authDomain: "realtimechatapp-7d707.firebaseapp.com",
+      
+        databaseURL: "https://realtimechatapp-7d707-default-rtdb.firebaseio.com",
+      
+        projectId: "realtimechatapp-7d707",
+      
+        storageBucket: "realtimechatapp-7d707.appspot.com",
+      
+        messagingSenderId: "151189490809",
+      
+        appId: "1:151189490809:web:09ddf0fc46c98d945918a8",
+      
+        measurementId: "G-RT9FL1EYY0"
+      
+      }
+      
+}
